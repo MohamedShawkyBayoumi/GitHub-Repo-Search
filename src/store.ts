30d5870e6e0ed5faa83d6b422613c0df72d31a7e
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-09-04 23:08:31 +0200

    add redux persist

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,18 @@
 import { createStore } from 'redux';
 import { globalReducer } from './reducers/global';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export const store = createStore(globalReducer)
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, globalReducer)
+
+let store: any = createStore(persistedReducer)
+let persistor = persistStore(store)
+
+export {
+    store, persistor
+}
