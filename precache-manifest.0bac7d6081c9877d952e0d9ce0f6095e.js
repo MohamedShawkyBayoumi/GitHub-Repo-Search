73d464b9f83af2db81144b9b878c84dd2b954619
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-09-07 00:35:19 +0200

    Updates

---
+++ precache-manifest.0bac7d6081c9877d952e0d9ce0f6095e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "febb79b90fc56cbe01d875c1552f4faf",
+    "url": "/GitHub-Repo-Search/index.html"
+  },
+  {
+    "revision": "096bb42f74640e904341",
+    "url": "/GitHub-Repo-Search/static/css/main.9b1ab89e.chunk.css"
+  },
+  {
+    "revision": "cd6d2e70e761454242b8",
+    "url": "/GitHub-Repo-Search/static/js/2.105a0da1.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/GitHub-Repo-Search/static/js/2.105a0da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096bb42f74640e904341",
+    "url": "/GitHub-Repo-Search/static/js/main.b7a26338.chunk.js"
+  },
+  {
+    "revision": "1e8b7864fe3d7bcd6d25",
+    "url": "/GitHub-Repo-Search/static/js/runtime-main.98bb28dd.js"
+  },
+  {
+    "revision": "b71bc541b323dcd8f0a887920f9670f9",
+    "url": "/GitHub-Repo-Search/static/media/github-logo.b71bc541.png"
+  }
+]);
